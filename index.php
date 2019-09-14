e803196ce22be598ec462d207dc3938bc147c636
codepipe <47555726+codepipe@users.noreply.github.com>
2019-09-14 11:23:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "uttam ganesh archana vinod pratik vishwas shivaraj vishwa  this is the aws devops automation class";
+echo "uttam ganesh archana vinod pratik vishwas shivaraj vishwa  this is the aws devops automation class added into the php file";
 ?>
 
 </body>
